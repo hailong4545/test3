69421b128565b42fe23d680f22e31c1191cb44d2
Phuong1301 <83696513+Phuong1301@users.noreply.github.com>
2021-10-21 17:03:08 +0700

    first commit

---
+++ untitled1/index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express();
+
+const server = require("http").createServer(app);
+const { Server } = require('socket.io')
+
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname+'/socket.html');
+    return;
+})
+
+users = []
+id = []
+
+io.on('connection', (socket) => {
+    socket.on('new person', (data) => {
+        id.push(socket.id);
+        socket.join(socket.id);
+        users.push(data);
+        io.emit('render', (users));
+    });
+
+    socket.on('send-request', (data) => {
+        for(let i = 0; i < users.length; i++) {
+            if (users[i] == data.name) {
+                console.log(id);
+                console.log(users);
+                io.to(id[i]).emit('recive-request', data);
+                break;
+            }
+        }
+    })
+})
+
+server.listen(3000);
